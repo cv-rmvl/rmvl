4dcb2acafc95d5f757215147d8188e1b7c3378b5
zhaoxi <535394140@qq.com>
2024-03-12 03:20:47 +0800

    统一第三方库依赖的配置方式

--- extra/group/src/gyro_group/init.cpp
+++ extra/group/src/gyro_group/init.cpp
@@ -31,7 +31,7 @@ GyroGroup::GyroGroup(const std::vector<combo::ptr> &first_combos, int armor_num)
     _tick = first_combos.front()->getTick();
     // 获取 RobotType
     std::vector<RobotType> robot_type_vec;
-    for_each(first_combos.begin(), first_combos.end(), [&robot_type_vec](combo::const_ptr val) {
+    for_each(first_combos.begin(), first_combos.end(), [&](combo::const_ptr val) {
         robot_type_vec.push_back(val->getType().RobotTypeID);
     });
     _type.RobotTypeID = calculateModeNum(robot_type_vec.begin(), robot_type_vec.end());
