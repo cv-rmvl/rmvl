e3b06be1e2168b732a2aa31be4e11a38f49a96f6
zhaoxi <535394140@qq.com>
2025-04-18 15:16:16 +0800

    (feat) algorithm: 增加拉格朗日乘子法支持

--- modules/algorithm/src/numcal.cpp
+++ modules/algorithm/src/numcal.cpp
@@ -11,19 +11,18 @@
 
 #ifdef HAVE_OPENCV
 #include <opencv2/core.hpp>
+#else
+#include <algorithm>
 #endif // HAVE_OPENCV
 
-#include "rmvl/algorithm/math.hpp"
 #include "rmvl/algorithm/numcal.hpp"
 #include "rmvl/core/util.hpp"
 
 #include "rmvlpara/algorithm.hpp"
 
-namespace rm
-{
+namespace rm {
 
-double Polynomial::operator()(double x) const noexcept
-{
+double Polynomial::operator()(double x) const noexcept {
     double y = _coeffs.back();
     std::for_each(_coeffs.rbegin() + 1, _coeffs.rend(), [&](double val) { y = y * x + val; });
     return y;
@@ -31,13 +30,11 @@ double Polynomial::operator()(double x) const noexcept
 
 ///////////////////// 函数插值 /////////////////////
 
-Interpolator::Interpolator(const std::vector<double> &xs, const std::vector<double> &ys)
-{
+Interpolator::Interpolator(const std::vector<double> &xs, const std::vector<double> &ys) {
     RMVL_Assert(xs.size() == ys.size());
     _xs = xs;
     _diffquot.resize(xs.size());
-    for (size_t i = 0; i < xs.size(); i++)
-    {
+    for (size_t i = 0; i < xs.size(); i++) {
         _diffquot[i].resize(i + 1);
         // 0 阶差商，即函数值
         _diffquot[i].front() = ys[i];
@@ -48,8 +45,7 @@ Interpolator::Interpolator(const std::vector<double> &xs, const std::vector<doub
             _diffquot[i][k] = (_diffquot[i][k - 1] - _diffquot[i - 1][k - 1]) / (_xs[i] - _xs[i - k]);
 }
 
-Interpolator &Interpolator::add(double x, double y)
-{
+Interpolator &Interpolator::add(double x, double y) {
     _xs.push_back(x);
     _diffquot.emplace_back(_xs.size());
     // 0 阶差商，即函数值
@@ -61,12 +57,10 @@ Interpolator &Interpolator::add(double x, double y)
     return *this;
 }
 
-double Interpolator::operator()(double x) const
-{
+double Interpolator::operator()(double x) const {
     double y{};
     double xprod{1.0};
-    for (size_t i = 0; i < _xs.size(); i++)
-    {
+    for (size_t i = 0; i < _xs.size(); i++) {
         y += _diffquot[i][i] * xprod;
         xprod *= (x - _xs[i]);
     }
@@ -75,8 +69,7 @@ double Interpolator::operator()(double x) const
 
 #ifdef HAVE_OPENCV
 
-CurveFitter::CurveFitter(const std::vector<double> &xs, const std::vector<double> &ys, std::bitset<8> order)
-{
+CurveFitter::CurveFitter(const std::vector<double> &xs, const std::vector<double> &ys, std::bitset<8> order) {
     RMVL_Assert(xs.size() == ys.size());
     if (order.count() > xs.size())
         RMVL_Error(RMVL_StsBadArg, "The number of 1 in \"order\" must be less than or equal to the number of nodes.");
@@ -88,10 +81,8 @@ CurveFitter::CurveFitter(const std::vector<double> &xs, const std::vector<double
     // 构建法方程系数矩阵 G
     int idx_size = static_cast<int>(_idx.size());
     cv::Mat G(idx_size, idx_size, CV_64FC1);
-    for (int i = 0; i < idx_size; ++i)
-    {
-        for (int j = 0; j < idx_size; ++j)
-        {
+    for (int i = 0; i < idx_size; ++i) {
+        for (int j = 0; j < idx_size; ++j) {
             G.at<double>(i, j) = 0.0;
             for (int k = 0; k < static_cast<int>(xs.size()); ++k)
                 G.at<double>(i, j) += std::pow(xs[k], _idx[i] + _idx[j]);
@@ -99,8 +90,7 @@ CurveFitter::CurveFitter(const std::vector<double> &xs, const std::vector<double
     }
     // 构建法方程系数矩阵 b
     cv::Mat b(idx_size, 1, CV_64FC1);
-    for (int i = 0; i < idx_size; ++i)
-    {
+    for (int i = 0; i < idx_size; ++i) {
         b.at<double>(i) = 0.0;
         for (std::size_t k = 0; k < xs.size(); ++k)
             b.at<double>(i) += std::pow(xs[k], _idx[i]) * ys[k];
@@ -112,8 +102,7 @@ CurveFitter::CurveFitter(const std::vector<double> &xs, const std::vector<double
     std::for_each(coeffs_mat.begin<double>(), coeffs_mat.end<double>(), [this](double val) { _coeffs.push_back(val); });
 }
 
-double CurveFitter::operator()(double x) const
-{
+double CurveFitter::operator()(double x) const {
     double retval{};
     for (std::size_t i = 0; i < _idx.size(); i++)
         retval += _coeffs[i] * std::pow(x, _idx[i]);
@@ -122,14 +111,12 @@ double CurveFitter::operator()(double x) const
 
 #else
 
-CurveFitter::CurveFitter(const std::vector<double> &, const std::vector<double> &, std::bitset<8>)
-{
+CurveFitter::CurveFitter(const std::vector<double> &, const std::vector<double> &, std::bitset<8>) {
     RMVL_Error(RMVL_StsBadFunc, "this function must be used with libopencv_core.so, please recompile "
                                 "RMVL by setting \"WITH_OPENCV=ON\" in CMake");
 }
 
-double CurveFitter::operator()(double) const
-{
+double CurveFitter::operator()(double) const {
     RMVL_Error(RMVL_StsBadFunc, "this function must be used with libopencv_core.so, please recompile "
                                 "RMVL by setting \"WITH_OPENCV=ON\" in CMake");
     return {};
@@ -137,11 +124,9 @@ double CurveFitter::operator()(double) const
 
 #endif // HAVE_OPENCV
 
-double NonlinearSolver::operator()(double x0, double eps, std::size_t max_iter) const
-{
+double NonlinearSolver::operator()(double x0, double eps, std::size_t max_iter) const {
     double xk{x0};
-    for (std::size_t i = 0; i < max_iter; i++)
-    {
+    for (std::size_t i = 0; i < max_iter; i++) {
         double yk = func(xk);
         if (std::abs(yk) < eps)
             break;
@@ -155,8 +140,7 @@ double NonlinearSolver::operator()(double x0, double eps, std::size_t max_iter) 
 RungeKutta::RungeKutta(
     const Odes &fs, const std::valarray<double> &p,
     const std::valarray<double> &lam, const std::valarray<std::valarray<double>> &r)
-    : _ks(p.size()), _fs(fs), _p(p), _lambda(lam), _r(r)
-{
+    : _ks(p.size()), _fs(fs), _p(p), _lambda(lam), _r(r) {
     // Initialize "ks"
     std::for_each(_ks.begin(), _ks.end(), [this](auto &k) { k.resize(_fs.size()); });
     // Check the size of p, lambda and R.rows
@@ -181,11 +165,9 @@ RungeKutta::RungeKutta(
  */
 static inline void calcRK(const std::valarray<std::valarray<double>> &r, const std::valarray<double> &p,
                           const std::valarray<double> &lambda, const Odes &fs, const double h,
-                          double &t, std::valarray<double> &x, std::vector<std::valarray<double>> &ks)
-{
+                          double &t, std::valarray<double> &x, std::vector<std::valarray<double>> &ks) {
     // 依次计算每个加权平均系数 k_i
-    for (std::size_t i = 0; i < ks.size(); i++)
-    {
+    for (std::size_t i = 0; i < ks.size(); i++) {
         // 计算 (a_i, k)
         std::valarray<double> inner_prod(fs.size());
         for (std::size_t n = 0; n < i; n++)
@@ -196,8 +178,7 @@ static inline void calcRK(const std::valarray<std::valarray<double>> &r, const s
     }
     // 更新 t 和 x
     t += h;
-    for (std::size_t j = 0; j < fs.size(); j++)
-    {
+    for (std::size_t j = 0; j < fs.size(); j++) {
         double inner_prod{};
         for (std::size_t i = 0; i < ks.size(); i++)
             inner_prod += h * lambda[i] * ks[i][j];
@@ -205,8 +186,7 @@ static inline void calcRK(const std::valarray<std::valarray<double>> &r, const s
     }
 }
 
-std::vector<std::valarray<double>> RungeKutta::solve(double h, std::size_t n)
-{
+std::vector<std::valarray<double>> RungeKutta::solve(double h, std::size_t n) {
     if (_x0.size() != _fs.size())
         RMVL_Error(RMVL_StsBadArg, "The initial value must be equal to the number of equations.");
     double t{_t0};
@@ -214,8 +194,7 @@ std::vector<std::valarray<double>> RungeKutta::solve(double h, std::size_t n)
     std::vector<std::valarray<double>> retval;
     retval.reserve(n + 1);
     retval.push_back(x);
-    for (std::size_t idx = 0; idx < n; idx++)
-    {
+    for (std::size_t idx = 0; idx < n; idx++) {
         calcRK(_r, _p, _lambda, _fs, h, t, x, _ks);
         // 保存结果
         retval.push_back(x);
@@ -224,14 +203,12 @@ std::vector<std::valarray<double>> RungeKutta::solve(double h, std::size_t n)
 }
 
 #if __cpp_lib_generator >= 202207L
-std::generator<std::valarray<double>> RungeKutta::generate(double h, std::size_t n)
-{
+std::generator<std::valarray<double>> RungeKutta::generate(double h, std::size_t n) {
     if (_x0.size() != _fs.size())
         RMVL_Error(RMVL_StsBadArg, "The initial must be equal to the number of equations.");
     double t{_t0};
     std::valarray<double> x{_x0};
-    for (std::size_t idx = 0; idx < n; idx++)
-    {
+    for (std::size_t idx = 0; idx < n; idx++) {
         calcRK(_r, _p, _lambda, _fs, h, t, x, _ks);
         co_yield x;
     }
