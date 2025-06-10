65a010ba7098e362a86f07a7f7787ac10a256262
zhaoxi <535394140@qq.com>
2025-06-11 00:26:31 +0800

    (feat) runin: 增加 run_in_list 支持
    1. 支持输入 run_in_list 列举出所有可用的编译镜像
    2. u22 和 u24 编译镜像增加中文支持

--- cmake/RMVLCompilerImages.cmake
+++ cmake/RMVLCompilerImages.cmake
@@ -9,6 +9,9 @@ execute_process(
 
 string(REPLACE "\n" ";" parse_result "${parse_result}")
 
+add_custom_target(run_in_list)
+
+set(img_num 0)
 foreach(line ${parse_result})
   string(REPLACE "#" ";" info "${line}")
   list(GET info 0 name)
@@ -16,44 +19,40 @@ foreach(line ${parse_result})
   list(GET info 2 options)
   list(GET info 3 cmd)
 
+  if("${name}" STREQUAL "list")
+    message(FATAL_ERROR "Docker image name 'list' is not allowed")
+  endif()
+
   add_custom_target(pull_${name}
     COMMAND bash -c "if ! docker image inspect ${image} > /dev/null 2>&1; then echo 'Pulling ${image}...' && docker pull ${image}; fi"
     COMMENT "Pulling Docker image '${name}' if needed"
     VERBATIM
   )
-  set(docker_command "docker run --rm -it \
-    -v ${PROJECT_SOURCE_DIR}:${PROJECT_SOURCE_DIR} \
-    -v ${PROJECT_SOURCE_DIR}/build/docker_images/${name}:${PROJECT_SOURCE_DIR}/build \
-    -w ${PROJECT_SOURCE_DIR} ${options} ${image} ${cmd}"
-  )
   add_custom_target(run_in_${name}
     COMMAND ${CMAKE_COMMAND} -E echo "Starting container: ${name} (based on image: ${image})"
-    COMMAND bash -c ${docker_command}
+    COMMAND docker run --rm -it
+      -v ${PROJECT_SOURCE_DIR}:${PROJECT_SOURCE_DIR}
+      -v ${PROJECT_SOURCE_DIR}/build/docker_images/${name}:${PROJECT_SOURCE_DIR}/build
+      -w ${PROJECT_SOURCE_DIR} ${options} ${image} ${cmd}
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     COMMENT "Running in ${name} container"
     DEPENDS pull_${name}
-    VERBATIM
+    VERBATIM USES_TERMINAL
   )
-endforeach()
 
-add_custom_target(run_in_list)
-
-foreach(line ${parse_result})
-  string(REPLACE "#" ";" info "${line}")
-  list(GET info 0 name)
-  list(GET info 1 image)
-  
+  math(EXPR img_num "${img_num} + 1")
   add_custom_command(
     TARGET run_in_list
-    COMMAND ${CMAKE_COMMAND} -E echo "  run_in_${name} - image: ${image}"
-    VERBATIM
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --blue "${img_num}. run_in_${name}"
+    COMMAND ${CMAKE_COMMAND} -E echo "   image: ${image}"
+    COMMAND ${CMAKE_COMMAND} -E echo "   options: ${options}"
+    COMMAND ${CMAKE_COMMAND} -E echo "   cmd: ${cmd}"
+    VERBATIM USES_TERMINAL
   )
 endforeach()
 
 add_custom_command(
-  TARGET run_in_list
-  PRE_BUILD
-  COMMAND ${CMAKE_COMMAND} -E echo "Available container targets:"
-  COMMENT "List of available run_in targets"
-  VERBATIM
+  TARGET run_in_list PRE_BUILD
+  COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --magenta --bold "Available compiler docker images, ${img_num} in total:"
+  VERBATIM USES_TERMINAL
 )
